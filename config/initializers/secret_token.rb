81b84fbc7da6626f59dc7dbaa77b4883d8253f6b
Antoine Lenoir <swissmadekzu@gmail.com>
2013-12-23 17:07:51 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tickets::Application.config.secret_key_base = 'dd7b963a2560cdcbf3190d2c7dd7c4ab0eb57c897ea7ca0eb2d9a5f80d3e72bf3cc3c8f01ef16b5cfca45c71e2f69e787cd2bdc670b1dfa6e2e7f9c17bb82417'
