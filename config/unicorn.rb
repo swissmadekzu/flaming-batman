c8fa8363bde5d15782cbfbe48dcd6154f70bb134
Antoine Lenoir <swissmadekzu@gmail.com>
2014-01-15 13:19:10 +0100

    Create unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/itsupport/ticketmanager"
+pid "/home/itsupport/ticketmanager/tmp/pids/unicorn.pid"
+stderr_path "/home/itsupport/ticketmanager/log/unicorn.log"
+stdout_path "/home/itsupport/ticketmanager/log/unicorn.log"
+
+listen "/tmp/unicorn.ticketmanager.sock"
+worker_processes 6
+timeout 30
