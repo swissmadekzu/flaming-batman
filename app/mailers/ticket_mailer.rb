b4c60dd13d5e25c237636f8ee53d22c494e3e5bb
itsupport <itsupport@itsupport-001.orbe.military-megastore.com>
2014-04-15 11:33:48 +0200

    v0.1.3

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -3,6 +3,8 @@ class TicketMailer < ActionMailer::Base
 
   def ticket_created(ticket)
     @ticket = ticket
+    mails_list = [@ticket.author.email]
+    techie_list.each {|em| mails_list << em}
     mail(to: @ticket.author.email, subject: "#{t("mailers.tickets.new_ticket")} ##{@ticket.id}")
   end
 
@@ -21,5 +23,10 @@ class TicketMailer < ActionMailer::Base
   def ticket_closed(ticket)
     @ticket = ticket
     mail(to: @ticket.author.email, subject: t("mailers.tickets.ticket_close", ticket_id: @ticket.id))
+  end
+
+  private
+  def techie_list
+    User.where(is_tech: true).map(&:email)
   end
 end
