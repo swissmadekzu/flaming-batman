1d82c3e6ece1640e7290ba07bbb4ef11f6705871
Antoine Lenoir <swissmadekzu@gmail.com>
2014-04-30 10:58:00 +0200

    Supervise ticket action

--- app/controllers/supervisor_controller.rb
+++ app/controllers/supervisor_controller.rb
@@ -5,6 +5,10 @@ class SupervisorController < ApplicationController
     @tickets = Ticket.all  
   end
   
+  def supervise
+    @ticket = Ticket.find(params[:id])
+  end
+  
   private
   def check_supervisor
     return false unless (current.user.is_supervisor? or current_user.is_tech?)
