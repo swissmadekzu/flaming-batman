87aea26235d1063ec9565673afba04a1bdf4a5b9
Antoine Lenoir <swissmadekzu@gmail.com>
2014-06-02 09:07:26 +0200

    API : Ajout de l'auteur

--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -2,12 +2,12 @@ class Api::TicketsController < ApplicationController
 
   def new
     @tickets = Ticket.where(is_invalid: false, aasm_state: "new")
-    render json: @tickets
+    render json: @tickets.to_json(include: :author)
   end
   
   def open
     @tickets = Ticket.where(is_invalid: false, aasm_state: "open")
-    render json: @tickets
+    render json: @tickets.to_json(include: :author)
   end
   
 end
